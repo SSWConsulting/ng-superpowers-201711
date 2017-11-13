49f4cad3745f5eaca7a5451e3ab3ab528509e35c
brendan richards <brendan@nobadthing.com>
2017-11-13 13:56:50 +1000

    added routiong

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,16 @@ import { CompanyListComponent } from './company/company-list/company-list.compon
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { CompanyService } from './company/company.service';
 
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
 import { CompanyTableComponent } from './company/company-table/company-table.component';
+import { CompanyEditComponent } from './company/company-edit/company-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CompanyListComponent,
-    CompanyTableComponent
+    CompanyTableComponent,
+    CompanyEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/company/company-edit/company-edit.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'fbc-company-edit',
+  templateUrl: './company-edit.component.html',
+  styleUrls: ['./company-edit.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CompanyEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
